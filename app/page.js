bad9da10a2863918d54e7a08efe7c983cd64101f
Mohammad Ali <alikhanak012345@gmail.com>
2025-10-12 22:46:46 +0500

    "Other to decimal functionality added"

--- app/page.js
+++ app/page.js
@@ -9,19 +9,28 @@ import { Code, Github, Linkedin, Menu, X } from "lucide-react"; // added Menu, X
 
 export default function Home() {
   const [inputValue, setInputValue] = useState("0");
+  const [OthertodecinputValue, setOthertodecInputValue] = useState("0");
   const [copiedText, setCopiedText] = useState("");
   const [ingredient, setIngredient] = useState("BINARY");
+  const [othertodecingredient, setothertodecingredient] = useState("BINARY");
+  const [fromOthertodecisActive, setfromOthertodecisActive] = useState(false);
   const [convertedvalues, setConvertedvalues] = useState({
     binary: "none",
     octal: "none",
     hexadecimal: "none",
   });
+  const [Othertodecconvertedvalues, setOthertodecconvertedvalues] = useState("0000");
   const [isOpen, setIsOpen] = useState(false); // sidebar state
 
   const handleCopy = (text) => {
     navigator.clipboard.writeText(text);
     setCopiedText(text);
   };
+  const handleTabs = () => {
+    setfromOthertodecisActive(!fromOthertodecisActive);
+    console.log(fromOthertodecisActive);
+  };
+  // dec to Other functions 
   let elementary_octal = {
     0: "000",
     1: "001",
@@ -128,7 +137,7 @@ export default function Home() {
       let d = int % 16;
       if (d > 9) {
         int_hex.push(alph_hex[d]);
-        console.log("here1")
+        console.log("here1");
       } else {
         if (int > 9) {
           int_hex.push(d);
@@ -143,15 +152,13 @@ export default function Home() {
         // int_octal.push(elementary_hex[int]);
         if (int > 9) {
           int_hex.push(alph_hex[int]);
-          console.log("here2")
-        }
-        else{
-        int_hex.push(int);
+          console.log("here2");
+        } else {
+          int_hex.push(int);
         }
         break;
       }
     }
-    console.log('int_hex', int_hex)
     return int_hex;
   }
   function fractionalpart_hex(frac) {
@@ -171,7 +178,6 @@ export default function Home() {
     }
     return frac_hex;
   }
-
   function integerpart_binary(int) {
     let int_binary = [];
     if (int == 0) {
@@ -224,6 +230,61 @@ export default function Home() {
       });
     }
   };
+
+// Other to dec functions
+function binary_to_decimal(binary_num) {
+  let decimal = 0;
+  for (let i = 0; i < binary_num.length; i++) {
+      decimal = decimal + binary_num.split('').reverse()[i] * Math.pow(2, i);
+}
+return decimal;
+}
+function octal_to_decimal(octal_num) {
+    let binary = [];
+  if (`${octal_num}`.includes(".")) {
+    let no_to_array = `${octal_num}`.split(".");
+    let int_part = no_to_array[0];
+    `${int_part}`.split("").forEach(e => {
+      binary.push(elementary_octal[e]);
+    });
+    binary.push(".");
+    let frac_part = no_to_array[1];
+    `${frac_part}`.split("").forEach(e => {
+      binary.push(elementary_octal[e]);
+    });
+    return(binary_to_decimal(binary.join("")));
+  }
+  else{
+    let int_part = octal_num;
+    `${int_part}`.split("").forEach(e => {
+      binary.push(elementary_octal[e]);
+    });
+    return(binary_to_decimal(binary.join("")));
+  }
+}
+function hex_to_decimal(hex_num) {
+    let binary = [];
+  if (hex_num.includes(".")) {
+    let no_to_array = hex_num.split(".");
+    let int_part = no_to_array[0];
+    `${int_part}`.split("").forEach(e => {
+      binary.push(elementary_hex[e]);
+    });
+    binary.push(".");
+    let frac_part = no_to_array[1];
+    `${frac_part}`.split("").forEach(e => {
+      binary.push(elementary_hex[e]);
+    });
+    return(binary_to_decimal(binary.join("")));
+  }
+  else{
+    let int_part = hex_num;
+    `${int_part}`.split("").forEach(e => {
+      binary.push(elementary_hex[e]);
+    });
+    return(binary_to_decimal(binary.join("")));
+  }
+}
 
   return (
     <div className="min-h-screen bg-gradient-to-br from-gray-50 to-gray-100 text-gray-800">
@@ -308,8 +369,6 @@ export default function Home() {
         </div>
       )}
 
-      {/* Main Content (unchanged) */}
-
       <main className="max-w-3xl mx-auto p-8 max-[400px]:p-4">
         {/* Heading */}
         <h2 className="text-3xl font-semibold mb-6 text-center max-[600px]:text-[24px]">
@@ -317,111 +376,267 @@ export default function Home() {
         </h2>
 
         {/* Card Section */}
-        <div className="bg-white shadow-md rounded-2xl p-6 space-y-6 max-[600px]:p-3">
-          {/* Radio options */}
-          <div className="flex flex-wrap gap-3">
-            <div className="flex align-items-center max-[600px]:text-[12px]  ">
-              <RadioButton
-                inputId="ingredient1"
-                name="conversion"
-                value="BINARY"
-                onChange={(e) => setIngredient(e.value)}
-                checked={ingredient === "BINARY"}
-              />
-              <label htmlFor="ingredient1" className="ml-2">
-                BINARY
-              </label>
-            </div>
-            <div className="flex align-items-center max-[600px]:text-[12px]  ">
-              <RadioButton
-                inputId="ingredient2"
-                name="conversion"
-                value="OCTAL"
-                onChange={(e) => setIngredient(e.value)}
-                checked={ingredient === "OCTAL"}
-              />
-              <label htmlFor="ingredient2" className="ml-2">
-                OCTAL
-              </label>
-            </div>
-            <div className="flex align-items-center max-[600px]:text-[12px]  ">
-              <RadioButton
-                inputId="ingredient3"
-                name="conversion"
-                value="HEXADECIMAL"
-                onChange={(e) => setIngredient(e.value)}
-                checked={ingredient === "HEXADECIMAL"}
-              />
-              <label htmlFor="ingredient3" className="ml-2">
-                HEXADECIMAL
-              </label>
-            </div>
+        <div className="">
+          <div className="bg-white shadow-md rounded-2xl h-[40px] mb-4 space-y-6 max-[600px]:p-3 w-[100%] flex items-center justify-around  relative overflow-hidden ">
+            {fromOthertodecisActive && (
+              <div
+                className="bg-gray-100  h-[100%] w-[49.8%] m-0 flex items-center justify-center absolute left-0 cursor-pointer"
+                onClick={handleTabs}
+              >
+                {" "}
+                From Decimal to other
+              </div>
+            )}
+            {fromOthertodecisActive == false && (
+              <div
+                className="bg-white  h-[100%] w-[49.8%] m-0 flex items-center justify-center absolute left-0 cursor-pointer"
+                onClick={handleTabs}
+              >
+                {" "}
+                From Decimal to other
+              </div>
+            )}
+            {fromOthertodecisActive && (
+              <div
+                className="bg-white  h-[100%] w-[49.8%] m-0 flex items-center justify-center absolute right-0 cursor-pointer"
+                onClick={handleTabs}
+              >
+                {" "}
+                From Other to Decimal
+              </div>
+            )}
+            {fromOthertodecisActive == false && (
+              <div
+                className="bg-gray-100  h-[100%] w-[49.8%] m-0 flex items-center justify-center absolute right-0 cursor-pointer"
+                onClick={handleTabs}
+              >
+                {" "}
+                From Other to Decimal
+              </div>
+            )}
           </div>
-
-          {/* Input + Submit */}
-          <div className="flex items-center ">
-            <FloatLabel className="w-[60%]">
-              <InputText
-                id="Decimal Number"
-                className="w-[90%]"
-                value={inputValue}
-                onChange={(e) => {
-                  const newValue = e.target.value;
-                  // Allow only numbers
-                  if (/^\d*\.?\d*$/.test(newValue)) {
-                    setInputValue(newValue);
-                  }
-                }}
-              />
-              <label htmlFor="username" className="max-[600px]:text-[14px]">
-                Decimal Number
-              </label>
-            </FloatLabel>
-            <Button
-              label="Submit"
-              icon="pi pi-check"
-              onClick={() => {
-                // conversion(inputValue);
-                if (ingredient == "BINARY") decimal_to_binary(inputValue);
-                else if (ingredient == "OCTAL") decimal_to_octal(inputValue);
-                else if (ingredient == "HEXADECIMAL")
-                  decimal_to_hex(inputValue);
-
-                // console.log(ingredient);
-              }}
-            />
-          </div>
-        </div>
-
-        {/* Fields with Copy buttons */}
-        <div className="mt-10 space-y-4">
-          {["Binary", "Octal", "Hexadecimal"].map((field, i) => {
-            const value = convertedvalues[field.toLowerCase()]; // dynamic property
-            return (
-              value !== "none" && (
-                <div
-                  key={i}
-                  className="flex items-center justify-between bg-white shadow-sm rounded-xl px-4 py-3 hover:shadow-md transition"
-                >
-                  <span>{`${field} Value : ${value}`}</span>
-                  <button
-                    onClick={() => handleCopy(value)}
-                    className="bg-green-600 text-white px-4 py-1.5 rounded-lg hover:bg-green-500 transition"
-                  >
-                    Copy
-                  </button>
+          {fromOthertodecisActive &&(
+          <div className="">
+            <div className="bg-white shadow-md rounded-2xl p-6 space-y-6 max-[600px]:p-3">
+              {/* Radio options */}
+              <div className="flex flex-wrap gap-3">
+                <div className="flex align-items-center max-[600px]:text-[12px]  ">
+                  <RadioButton
+                    inputId="ingredient1"
+                    name="conversion"
+                    value="BINARY"
+                    onChange={(e) => setothertodecingredient(e.value)}
+                    checked={othertodecingredient === "BINARY"}
+                  />
+                  <label htmlFor="ingredient1" className="ml-2">
+                    BINARY
+                  </label>
                 </div>
-              )
-            );
-          })}
-        </div>
+                <div className="flex align-items-center max-[600px]:text-[12px]  ">
+                  <RadioButton
+                    inputId="ingredient2"
+                    name="conversion"
+                    value="OCTAL"
+                    onChange={(e) => setothertodecingredient(e.value)}
+                    checked={othertodecingredient === "OCTAL"}
+                  />
+                  <label htmlFor="ingredient2" className="ml-2">
+                    OCTAL
+                  </label>
+                </div>
+                <div className="flex align-items-center max-[600px]:text-[12px]  ">
+                  <RadioButton
+                    inputId="ingredient3"
+                    name="conversion"
+                    value="HEXADECIMAL"
+                    onChange={(e) => setothertodecingredient(e.value)}
+                    checked={othertodecingredient === "HEXADECIMAL"}
+                  />
+                  <label htmlFor="ingredient3" className="ml-2">
+                    HEXADECIMAL
+                  </label>
+                </div>
+              </div>
 
-        {/* Copy confirmation */}
-        {copiedText && (
-          <p className="mt-6 text-green-700 text-center font-medium">
-            Copied: <span className="font-semibold">{copiedText}</span>
-          </p>
-        )}
+              {/* Input + Submit */}
+              <div className="flex items-center ">
+                <FloatLabel className="w-[60%]">
+                  <InputText
+                    id={`${othertodecingredient.toLowerCase()} Number`}
+                    className="w-[90%]"
+                    value={OthertodecinputValue}
+                    onChange={(e) => {
+                      const newValue = e.target.value;
+                      // Allow only numbers
+                      if (othertodecingredient == "BINARY") {
+                        if (/^[01]*$/.test(newValue)) {
+                          setOthertodecInputValue(newValue);
+                        }
+                      } else if (othertodecingredient == "OCTAL") {
+                        if (/^[0-7]*$/.test(newValue)) {
+                          setOthertodecInputValue(newValue);
+                        }
+                      } else {
+                        if (/^[0-9a-fA-F]*$/.test(newValue)) {
+                          setOthertodecInputValue(newValue.toUpperCase());
+                        }
+                      }
+                    }}
+                  />
+                  <label htmlFor="username" className="max-[600px]:text-[14px]">
+                    {`${othertodecingredient.toLowerCase()}`} Number
+                  </label>
+                </FloatLabel>
+                <Button
+                  label="Submit"
+                  icon="pi pi-check"
+                  onClick={() => {
+                    // conversion(inputValue);
+                    if (othertodecingredient == "BINARY") setOthertodecconvertedvalues(binary_to_decimal(OthertodecinputValue));
+                    else if (othertodecingredient == "OCTAL")
+                      setOthertodecconvertedvalues(octal_to_decimal(OthertodecinputValue));
+                    else if (othertodecingredient == "HEXADECIMAL")
+                      setOthertodecconvertedvalues(hex_to_decimal(OthertodecinputValue));
+
+                    // console.log(ingredient);
+                  }}
+                />
+              </div>
+            </div>
+
+            {/* Fields with Copy buttons */}
+            <div className="mt-10 space-y-4">
+              {["Decimal"].map((field, i) => {
+                const value = Othertodecconvertedvalues; // dynamic property
+                return (
+                  value !== "none" && (
+                    <div
+                      key={i}
+                      className="flex items-center justify-between bg-white shadow-sm rounded-xl px-4 py-3 hover:shadow-md transition"
+                    >
+                      <span>{`${field} Value : ${value}`}</span>
+                      <button
+                        onClick={() => handleCopy(value)}
+                        className="bg-green-600 text-white px-4 py-1.5 rounded-lg hover:bg-green-500 transition"
+                      >
+                        Copy
+                      </button>
+                    </div>
+                  )
+                );
+              })}
+            </div>
+          </div>)}
+          {fromOthertodecisActive == false &&(
+          <div className="">
+            <div className="bg-white shadow-md rounded-2xl p-6 space-y-6 max-[600px]:p-3">
+              {/* Radio options */}
+              <div className="flex flex-wrap gap-3">
+                <div className="flex align-items-center max-[600px]:text-[12px]  ">
+                  <RadioButton
+                    inputId="ingredient1"
+                    name="conversion"
+                    value="BINARY"
+                    onChange={(e) => setIngredient(e.value)}
+                    checked={ingredient === "BINARY"}
+                  />
+                  <label htmlFor="ingredient1" className="ml-2">
+                    BINARY
+                  </label>
+                </div>
+                <div className="flex align-items-center max-[600px]:text-[12px]  ">
+                  <RadioButton
+                    inputId="ingredient2"
+                    name="conversion"
+                    value="OCTAL"
+                    onChange={(e) => setIngredient(e.value)}
+                    checked={ingredient === "OCTAL"}
+                  />
+                  <label htmlFor="ingredient2" className="ml-2">
+                    OCTAL
+                  </label>
+                </div>
+                <div className="flex align-items-center max-[600px]:text-[12px]  ">
+                  <RadioButton
+                    inputId="ingredient3"
+                    name="conversion"
+                    value="HEXADECIMAL"
+                    onChange={(e) => setIngredient(e.value)}
+                    checked={ingredient === "HEXADECIMAL"}
+                  />
+                  <label htmlFor="ingredient3" className="ml-2">
+                    HEXADECIMAL
+                  </label>
+                </div>
+              </div>
+
+              {/* Input + Submit */}
+              <div className="flex items-center ">
+                <FloatLabel className="w-[60%]">
+                  <InputText
+                    id="Decimal Number"
+                    className="w-[90%]"
+                    value={inputValue}
+                    onChange={(e) => {
+                      const newValue = e.target.value;
+                      // Allow only numbers
+                      if (/^\d*\.?\d*$/.test(newValue)) {
+                        setInputValue(newValue);
+                      }
+                    }}
+                  />
+                  <label htmlFor="username" className="max-[600px]:text-[14px]">
+                    Decimal Number
+                  </label>
+                </FloatLabel>
+                <Button
+                  label="Submit"
+                  icon="pi pi-check"
+                  onClick={() => {
+                    // conversion(inputValue);
+                    if (ingredient == "BINARY") decimal_to_binary(inputValue);
+                    else if (ingredient == "OCTAL")
+                      decimal_to_octal(inputValue);
+                    else if (ingredient == "HEXADECIMAL")
+                      decimal_to_hex(inputValue);
+
+                    // console.log(ingredient);
+                  }}
+                />
+              </div>
+            </div>
+
+            {/* Fields with Copy buttons */}
+            <div className="mt-10 space-y-4">
+              {["Binary", "Octal", "Hexadecimal"].map((field, i) => {
+                const value = convertedvalues[field.toLowerCase()]; // dynamic property
+                return (
+                  value !== "none" && (
+                    <div
+                      key={i}
+                      className="flex items-center justify-between bg-white shadow-sm rounded-xl px-4 py-3 hover:shadow-md transition"
+                    >
+                      <span>{`${field} Value : ${value}`}</span>
+                      <button
+                        onClick={() => handleCopy(value)}
+                        className="bg-green-600 text-white px-4 py-1.5 rounded-lg hover:bg-green-500 transition"
+                      >
+                        Copy
+                      </button>
+                    </div>
+                  )
+                );
+              })}
+            </div>
+          </div>)}
+
+          {/* Copy confirmation */}
+          {copiedText && (
+            <p className="mt-6 text-green-700 text-center font-medium">
+              Copied: <span className="font-semibold">{copiedText}</span>
+            </p>
+          )}
+        </div>
       </main>
     </div>
   );
